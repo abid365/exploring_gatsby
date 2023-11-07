dbbe8b602880969cee6bea5c0cda77c9f157aabd
jawad Bin Azam <abidibnazam@gmail.com>
2023-11-07 14:21:19 +0600

    working with css module

---
+++ src/pages/about.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle={"About page"}>
+      <p>
+        Hi there! I'm the proud creator of this site, which I built with Gatsby.
+      </p>
+    </Layout>
+  );
+};
+
+export const Head = () => (
+  <>
+    <title>About Page</title>
+    <meta name="description" content="your description" />
+  </>
+);
+
+export default AboutPage;
